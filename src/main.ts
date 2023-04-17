0d7feba1758cae265b1738df9cdce203a302da5b
arvindpamuri <aravind.pamuri@gmail.com>
2023-04-17 10:25:06 -0500

    listen port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000; // Get port from environment variable or use default 3000
+  await app.listen(port, () => {
+    console.log(`App is listening on port number ${port}`);
+  });
 }
+
 bootstrap();
